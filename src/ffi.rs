4ea74e2a3bc09be9f2ec6bc44f8de5674adecf37
Eduardo Lima (Etrunko) <eblima@gmail.com>
2025-02-27 15:36:14 -0300

    Add forecast options to C interface
    Initially with supoort for setting location (Lat, Long) only.

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,4 +1,6 @@
 use crate::Client;
+use crate::Location;
+use crate::forecast::Options;
 
 #[no_mangle]
 pub extern "C" fn open_meteo_client_new() -> *mut Client {
@@ -10,5 +12,23 @@ pub extern "C" fn open_meteo_client_new() -> *mut Client {
 pub extern "C" fn open_meteo_client_free(client: *mut Client) {
     unsafe {
         drop(Box::from_raw(client));
+    };
+}
+
+#[no_mangle]
+pub extern "C" fn open_meteo_forecast_options_new(lat: f64, lng: f64) -> *mut Options {
+    let mut opts = Options::default();
+    opts.location = Location {
+        lat: lat,
+        lng: lng,
+    };
+    Box::into_raw(Box::new(opts))
+}
+
+#[no_mangle]
+pub extern "C" fn open_meteo_forecast_options_set_location(opts: &mut Options, lat: f64, lng: f64) {
+    opts.location = Location {
+        lat: lat,
+        lng: lng,
     };
 }
