028f4da04b61f19ead953cf6d997c7f88abe5207
Eduardo Lima (Etrunko) <eblima@gmail.com>
2025-03-03 13:09:03 -0300

    ffi: Retrieve current forecast

--- src/ffi.rs
+++ src/ffi.rs
@@ -26,6 +26,8 @@ pub extern "C" fn open_meteo_forecast_options_new(lat: f64, lng: f64) -> *mut Op
         lat: lat,
         lng: lng,
     };
+
+    opts.current.push("temperature_2m".into());
     Box::into_raw(Box::new(opts))
 }
 
